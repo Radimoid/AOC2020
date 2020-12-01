b3934168538c965f8256d68cec1689fcd078b953
Radim Langer <langer@adash.cz>
2020-12-01 13:48:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AOC2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
